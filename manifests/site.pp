8cb3ad94838524ab9dcd4c4c5124b2d227593f54
Frederik Wagner <wagner@wagit.de>
2014-09-22 12:24:53 +0200

    added omd site creation

---
+++ manifests/site.pp
@@ -0,0 +1,51 @@
+# == Define: omd::site
+#
+# This define creates a OMD site.
+#
+# This works on Debian and RedHat like systems.
+# Puppet Version >= 3.4.0
+#
+# === Parameters
+#
+# [*repo*]
+#   Which repo to use stable/testing
+#   defaults to _stable_
+#
+# === Examples
+#
+# omd::site { 'default': }
+#
+# === Authors
+#
+# Frederik Wagner <wagner@wagit.de>
+#
+# === Copyright
+#
+# Copyright 2014 Frederik Wagner
+#
+define omd::site (
+  $uid = undef,
+  $gid = undef,
+) {
+  if $uid {
+    validate_re($uid, '\d+')
+    $_uid = "--uid ${uid} "
+  }
+  if $gid {
+    validate_re($gid, '\d+')
+    $_gid = "--gid ${gid} "
+  }
+
+  require omd
+
+  Exec {
+    path => ['/bin', '/usr/bin']
+  }
+
+
+  exec { "create omd site: ${name}":
+    command => "omd create ${_uid}${_gid}${name}",
+    unless  => "omd sites -b | grep -q '\\<${name}\\>'",
+  }
+
+}
--- spec/defines/site_spec.rb
+++ spec/defines/site_spec.rb
@@ -3,5 +3,39 @@ require 'spec_helper'
 describe 'omd::site' do
     let(:title) { 'default' }
 
+    it { is_expected.to contain_omd__site('default') }
+
+    it { is_expected.to contain_class('omd') }
+
+    it do
+      is_expected.to contain_exec('create omd site: default').with({
+        :command  => 'omd create default',
+        :unless   => 'omd sites -b | grep -q \'\\<default\\>\'',
+        :path     => ['/bin', '/usr/bin'],
+      })
+    end
+
+    context 'with parameter uid => 678' do
+      let(:params) {{ :uid => 678 }}
+      it { is_expected.to contain_exec('create omd site: default').with_command(
+             'omd create --uid 678 default')
+      }
+    end
+    context 'with parameter uid => breakme' do
+      let(:params) {{ :uid => 'breakme' }}
+      it { is_expected.to raise_error(/does not match/) }
+    end
+
+    context 'with parameter gid => 789' do
+      let(:params) {{ :gid => 789 }}
+      it { is_expected.to contain_exec('create omd site: default').with_command(
+             'omd create --gid 789 default')
+      }
+    end
+    context 'with parameter gid => breakme' do
+      let(:params) {{ :uid => 'breakme' }}
+      it { is_expected.to raise_error(/does not match/) }
+    end
+
 
 end
