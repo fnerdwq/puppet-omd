5b385e03e785c799f5d799ce6a186750db9d0c29
Frederik Wagner <wagner@wagit.de>
2014-10-07 13:21:35 +0200

    added client::checks::cert

---
+++ manifests/client/checks/cert.pp
@@ -0,0 +1,72 @@
+# == Class: omd::client::checks::cert
+#
+# This class installs a ssl cert check over MRPE.
+#
+# Check script from: <https://github.com/ripienaar/monitoring-scripts>
+#
+# === Parameters
+#
+# [*name*]
+#   Name of certificate, where title is used for resource description.
+#   defaults to _$title_
+#
+# [*warn*]
+#   Warn level in seconds
+#   defaults to _3600_
+#
+# [*crit*]
+#   Critical level in seconds
+#   defaults to _7200_
+#
+# [*options*]
+#   Further options of check (see --help)
+#   defaults to _--cert /etc/ssl/certs/ssl-cert-snakeoil.pem_
+#
+# === Examples
+#
+# include omd::client::checks::cert
+#
+# === Authors
+#
+# Frederik Wagner <wagner@wagit.de>
+#
+# === Copyright
+#
+# Copyright 2014 Frederik Wagner
+#
+define omd::client::checks::cert (
+  $warn    = '3600',
+  $crit    = '7200',
+  $options = '',
+) {
+  validate_re($name, '^[^\s]+$')
+  validate_re($warn, '^\d+$')
+  validate_re($crit, '^\d+$')
+  validate_string($options)
+
+  include 'omd::client::checks'
+
+  $title_repl = regsubst($title, '[/\s]', '_', 'G')
+
+  $plugin_path = $omd::client::checks::params::plugin_path
+  $content = "Cert_${title_repl}\t${plugin_path}/nagios/plugins/check_cert.rb -w ${warn} -c ${crit} --cert ${name} ${options}\n"
+  concat::fragment { "check_cert_${title_repl}":
+    target  => $omd::client::checks::params::mrpe_config,
+    content => $content,
+    order   => '50',
+    require => File['check_cert'],
+  }
+
+  # reinventorize trigger if a MRPE check changed
+  @@file { "${::puppet_vardir}/omd/check_cert_${title_repl}_${::fqdn}":
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => $content,
+    backup  => false,
+    # server collection and site inventory trigger tags
+    tag     => ['omd_client_checks', "omd_client_check_${::fqdn}"],
+  }
+
+}
---
+++ spec/defines/client__checks__cert_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe 'omd::client::checks::cert' do
+  let(:title) { '/etc/ssl/certs/ssl-cert-snakeoil.pem' }
+
+  # we need to set the must parameter
+  let(:pre_condition) { 'class omd::client { $check_mk_version = "1.2.3" }' }
+
+  it { is_expected.to contain_class('omd::client::checks') }
+
+  it do
+    is_expected.to contain_concat__fragment('check_cert__etc_ssl_certs_ssl-cert-snakeoil.pem').with({
+      :target  => '/etc/check_mk/mrpe.cfg',
+      :content => "Cert__etc_ssl_certs_ssl-cert-snakeoil.pem\t/usr/local/lib/nagios/plugins/check_cert.rb -w 3600 -c 7200 --cert /etc/ssl/certs/ssl-cert-snakeoil.pem \n",
+      :order   => '50',
+    }).that_requires('File[check_cert]')
+  end
+
+  context 'with title => A Cert.pem and parameter name => /path/to/ACert.pem' do
+    let(:title) { 'A Cert.pem' }
+    let(:params) {{ :name => '/path/to/ACert.pem' }}
+
+    it do
+      is_expected.to contain_concat__fragment('check_cert_A_Cert.pem').with_content(
+        "Cert_A_Cert.pem\t/usr/local/lib/nagios/plugins/check_cert.rb -w 3600 -c 7200 --cert /path/to/ACert.pem \n")
+    end
+  end
+  context 'parameter name => break me' do
+    let(:params) {{ :name => 'break me' }}
+    it { is_expected.to raise_error(/does not match/) }
+  end
+
+  context 'with parameter warn => 14400' do
+    let(:params) {{ :warn => 14400 }}
+      it { is_expected.to contain_concat__fragment('check_cert__etc_ssl_certs_ssl-cert-snakeoil.pem').with_content(/-w 14400/)
+    }
+  end
+  context 'with parameter warn => breakme' do
+    let(:params) {{ :warn => 'breakme' }}
+      it { is_expected.to raise_error(/does not match/)
+    }
+  end
+
+  context 'with parameter crit => 14400' do
+    let(:params) {{ :crit => 14400 }}
+      it { is_expected.to contain_concat__fragment('check_cert__etc_ssl_certs_ssl-cert-snakeoil.pem').with_content(/-c 14400/)
+    }
+  end
+  context 'with parameter crit => breakme' do
+    let(:params) {{ :crit => 'breakme' }}
+      it { is_expected.to raise_error(/does not match/)
+    }
+  end
+
+  context 'with parameter options => \'-f -e\'' do
+    let(:params) {{ :options => '-f -e'}}
+      it { is_expected.to contain_concat__fragment('check_cert__etc_ssl_certs_ssl-cert-snakeoil.pem').with_content(/-f -e/)
+    }
+  end
+  context 'with parameter options => {}' do
+    let(:params) {{ :options => {} }}
+      it { is_expected.to raise_error(/is not a string/)
+    }
+  end
+
+  describe 'reinventorize trigger export' do
+    # not testable for the moment
+  end
+end
